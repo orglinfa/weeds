3212ae3048cbdcdc944184f7bf84b67ce557b352
orglinfa <lingfeian@aliyun.com>
2018-07-26 07:48:28 +0800

    first commit

---
+++ src/main/java/org/lfa/weeds/WeedsApplication.java
@@ -0,0 +1,12 @@
+package org.lfa.weeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeedsApplication.class, args);
+    }
+}
---
+++ src/test/java/org/lfa/weeds/WeedsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lfa.weeds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeedsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
